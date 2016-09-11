187c8bd79aea5d31cb8525eae12f1bc8b4d2bd01
Robin Munn <rmunn@pobox.com>
2016-09-11 06:00:14 -0500

    Markdown refactor, step 3

--- Markdown.fs
+++ Markdown.fs
@@ -41,6 +41,66 @@ let isListItem s = s |> startsWith "* " && s.Length > 2
 
 let stripListItemMarkdown (s:string) = s.Substring(2)
 
+let findBoldMarker (fromIdx:int) (s:string) =
+    s.IndexOf("__",fromIdx)
+let findItalicMarker (fromIdx:int) (s:string) =
+    s.IndexOf("_", fromIdx)
+
+let hasBoldMarker   s = findBoldMarker   0 s > -1
+let hasItalicMarker s = findItalicMarker 0 s > -1
+
+let findBoldSpan s =
+    if hasBoldMarker s then
+        let startIdx = findBoldMarker 0 s
+        let endIdx = findBoldMarker (startIdx+2) s
+        if endIdx = -1 then None else Some (startIdx,endIdx)
+    else
+        None
+
+let findItalicSpan s =
+    if hasItalicMarker s then
+        let startIdx = findItalicMarker 0 s
+        let endIdx = findItalicMarker (startIdx+1) s
+        if endIdx = -1 then None else Some (startIdx,endIdx)
+    else
+        None
+
+let extractBoldSpan s =
+    match findBoldSpan s with
+    | None -> None
+    | Some (startIdx,endIdx) ->
+        Some (startIdx,endIdx,s.[startIdx+2 .. endIdx-1])
+
+let extractItalicSpan s =
+    match findItalicSpan s with
+    | None -> None
+    | Some (startIdx,endIdx) ->
+        Some (startIdx,endIdx,s.[startIdx+1 .. endIdx-1])
+
+let substrBefore idx (s:string) =
+    if idx <= 0 then "" else s.[..idx-1]
+
+let substrAfter idx (s:string) =
+    if idx >= s.Length then "" else s.[idx..]
+
+let convertBoldSpan s =
+    match extractBoldSpan s with
+    | None -> s
+    | Some (startIdx,endIdx,content) ->
+        sprintf "%s<em>%s</em>%s"
+            (s |> substrBefore startIdx)
+            content
+            (s |> substrAfter (endIdx+2))
+
+let convertItalicSpan s =
+    match extractItalicSpan s with
+    | None -> s
+    | Some (startIdx,endIdx,content) ->
+        sprintf "%s<i>%s</i>%s"
+            (s |> substrBefore startIdx)
+            content
+            (s |> substrAfter (endIdx+1))
+
 let convertListItem s =
     let content = s |> stripListItemMarkdown
     let convertedContent =
@@ -54,10 +114,9 @@ let convertListItem s =
 
 let rec parse (markdown: string) =
     let mutable html = ""
-    let mutable remainder = markdown
     let mutable isList = false
 
-    let lines = remainder.Split('\n')
+    let lines = markdown.Split('\n')
 
     for i = 0 to lines.Length - 1 do
 
@@ -71,38 +130,11 @@ let rec parse (markdown: string) =
             html <- html + convertHeader lines.[i]
 
         else
-            let mutable line = lines.[i]
-            let mutable __pos = line.IndexOf "__"
-
-            while __pos > -1 do
-                let mutable __pos' = if __pos >= (line.Length - 2) then -1 else line.IndexOf("__", __pos + 2)
-
-                if __pos' > -1 then
-                    if __pos + 2 >= (line.Length - 1) then
-                        line <- line.[0.. __pos - 1] + "<em>" + line.[__pos + 2 .. __pos' - 1] + "</em>"
-                        __pos <- __pos' + 1
-                    else
-                        line <- line.[0.. __pos - 1] + "<em>" + line.[__pos + 2 .. __pos' - 1] + "</em>" + line.[__pos' + 2 ..]
-                        __pos <- __pos' + 1
-                else
-                    __pos <- -1
-
-            __pos <- line.IndexOf "_"
-
-            while __pos > -1 do
-                let mutable __pos' = if __pos >= (line.Length - 1) then -1 else line.IndexOf("_", __pos + 1)
-
-                if __pos' > -1 then
-                    if __pos + 1 >= (line.Length - 1) then
-                        line <- line.[0.. __pos - 1] + "<i>" + line.[__pos + 1 .. __pos' - 1] + "</i>"
-                        __pos <- __pos' + 1
-                    else
-                        line <- line.[0.. __pos - 1] + "<i>" + line.[__pos + 1 .. __pos' - 1] + "</i>" + line.[__pos' + 1 ..]
-                        __pos <- __pos' + 1
-                else
-                    __pos <- -1
-
-            html <- html + "<p>" + line + "</p>"
+            let content =
+                lines.[i]
+                |> convertBoldSpan
+                |> convertItalicSpan
+            html <- html + convertParagraph content
 
     if isList then
         html <- html + "</ul>"
